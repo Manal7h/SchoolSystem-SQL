5903f43e584717692d9127d0710833088c391203
manal7h <manal.alsenani22@gmail.com>
2022-12-29 00:09:30 -0800

    create student table in java

--- src/SQLTask.java
+++ src/SQLTask.java
@@ -1,9 +1,70 @@
+import java.sql.*;
+import java.util.Scanner;
 
 public class SQLTask {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
+        // Creating the connection using Oracle DB
+        // Note: url syntax is standard, so do grasp
+        String url = "jdbc:sqlserver://192.168.100.100:1433;databaseName=SchoolMgt;encrypt=true;trustServerCertificate=true";
 
+        // Username and password to access DB
+        // Custom initialization
+        String user = "sa";
+        String pass = "root";
+
+        // Entering the data
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("enter id");
+        Integer id = scanner.nextInt();
+
+        System.out.println("enter first name");
+        String fname = scanner.next();
+        
+        System.out.println("enter last name");
+        String lname = scanner.next();
+
+        System.out.println("enter birthdate");
+        String birthdate = scanner.next();
+
+        // Inserting data using SQL query
+        String sql = "insert into Students values(" + id + ",'" + fname + "','" + lname + "','" + birthdate + "')";
+
+        // Connection class object
+        Connection con = null;
+
+        // Try block to check for exceptions
+        try {
+
+            Driver driver = (Driver) Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver").newInstance();
+            // Registering drivers
+            DriverManager.registerDriver(driver);
+
+            // Reference to connection interface
+            con = DriverManager.getConnection(url, user,
+                    pass);
+
+            // Creating a statement
+            Statement st = con.createStatement();
+
+            // Executing query
+            int m = st.executeUpdate(sql);
+            if (m >=  1)
+                System.out.println(
+                        "inserted successfully : " + sql);
+            else
+                System.out.println("insertion failed");
+
+            // Closing the connections
+            con.close();
+        }
+
+        // Catch block to handle exceptions
+        catch (Exception ex) {
+            // Display message when exceptions occurs
+            System.err.println(ex);
+        }
+}
 }
